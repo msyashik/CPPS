9578e30b5870ebc967edea204330b9d2e4093cef
msyashik <44289812+msyashik@users.noreply.github.com>
2019-07-04 23:25:42 +0600

    Create CHFIDEAL.cpp

---
+++ Codechef/CHFIDEAL.cpp
@@ -0,0 +1,26 @@
+// Problem Link: https://www.codechef.com/problems/CHFIDEAL
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long 
+#define Fastest ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+
+int main()
+{
+	//Fastest
+	
+	int a;
+	
+	cout << 3 << endl;
+    cout.flush();	
+	cin >> a;
+	
+	if(a == 1) cout << 2 << endl;
+	else cout << 1 << endl;
+	
+	return 0;
+}
+https://www.codechef.com/problems/CHFIDEAL
