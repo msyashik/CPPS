db4f40d20546ab2276eb3cef4a3a562de4b96f8d
msyashik <44289812+msyashik@users.noreply.github.com>
2019-07-18 13:27:30 +0600

    Create 12952.cpp

---
+++ UVA/12952.cpp
@@ -0,0 +1,20 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long 
+#define Coming ios_base :: sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define endl "\n"
+
+int main()
+{
+	
+	int a, b;
+	
+	while(scanf("%d %d",&a, &b) == 2)
+	{
+		cout << max(a,b) << endl;
+	}
+	return 0;
+}
