1b26ebdfc8b8a2c2f5d6f1d610a06c9edd0203fd
msyashik <44289812+msyashik@users.noreply.github.com>
2019-07-30 18:33:39 +0600

    Create LCS(Recursive).cpp

---
+++ Personal/LCS(Recursive).cpp
@@ -0,0 +1,59 @@
+//LCS(recursive dp)
+//Shafayets
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[3005][3005];
+string a, b;
+int len_a, len_b;
+string ss = "";	
+
+int LCS(int i, int j)
+{
+	if(i == len_a || j == len_b) return 0;
+	if(dp[i][j] != -1) return dp[i][j];
+	
+	int ans = 0;
+	if(a[i] == b[j]) ans = 1 + LCS(i+1, j+1);
+	else
+	{
+		int ans1 = LCS(i, j+1);
+		int ans2 = LCS(i+1, j);
+		ans = max(ans1, ans2);
+	}
+	return dp[i][j] = ans;
+}
+
+void printLCS(int i, int j)
+{
+	if(i == len_a || j == len_b)
+	{
+		cout << ss << endl; //LCS string
+		return;
+	}
+	if(a[i] == b[j])
+	{
+		ss += a[i];
+		printLCS(i+1, j+1);
+	}
+	else
+	{
+		if(dp[i][j+1] > dp[i+1][j]) printLCS(i,j+1);
+		else printLCS(i+1,j);
+	}
+	return;
+}
+
+int main()
+{
+    memset(dp, -1, sizeof(dp));	
+	cin >> a >> b;
+	len_a = a.size() , len_b = b.size();	
+	int sum = LCS(0,0);
+	
+	cout << sum << endl; //LCS length
+	printLCS(0,0);
+	return 0;
+}
