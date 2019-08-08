bc55d2848f8577a15fce49e5fd042d3951ebc5bc
msyashik <44289812+msyashik@users.noreply.github.com>
2019-08-08 10:00:36 +0600

    Update (ETF - Euler Totient Function).cpp

--- SPOJ/(ETF - Euler Totient Function).cpp
+++ SPOJ/(ETF - Euler Totient Function).cpp
@@ -1,69 +1,78 @@
-//Euler Totient function
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define ll long long
+#define ll long long 
+#define Fastest ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define SIZE 1000005
 
-void pf(int n)
+char sieve[SIZE];
+vector<int>prime;
+
+void primeSieve()
 {
-	ll ans  = n;
-    	
-	if(n%2 == 0)
-	{
-		while(n%2 == 0)
-		{
-			n /= 2;
-		}
-		
-		ans /= 2;
-		
-		ans *= (2-1);
-	}
-	
-	int len = sqrt(n);
-	
-	
+	sieve[0] = 1;
+	sieve[1] = 1;
+	for(int i = 4; i <= 1000000; i+=2) sieve[i] = 1;
+	int len = sqrt(1000000);
 	for(int i = 3; i <= len; i+=2)
 	{
-		if(n%i == 0)
+		if(sieve[i] == 0)
 		{
-			while(n%i == 0)
-		    {
-				n /= i;
+			for(int j = i*i; j <= 1000000; j += 2*i)
+			{
+				sieve[j] = 1;
 			}
-			
-			ans /= i;
-			
-			ans *= (i-1);
 		}
 	}
-	
-	if(n > 2)
+	prime.push_back(2);
+	for(int i = 3; i <= 1000000; i+=2)
 	{
-		ans /= n;
-		
-		ans *= (n-1);
+		if(sieve[i] == 0) prime.push_back(i);
 	}
-	
-	printf("%lld\n", ans);
-	
 }
 
+void div(int n)
+{
+    int len = sqrt(n),total = n;
+    for(int i = 0; i < prime.size() && prime[i] <= len; i++)
+    {
+		if(sieve[n] == 0) break;
+		if(n%prime[i] == 0)
+		{
+			while(n%prime[i] == 0)
+			{
+				n /= prime[i];
+			}
+			len = sqrt(n);
+			total /= prime[i];
+			total *= prime[i]-1;
+		}
+	}
+	if(n != 1)
+	{
+		total /= n;
+		total *= n-1;
+	}	
+	cout << total << endl;
+}
 
 int main()
 {
-	int t, a;
+	Fastest
 	
-	scanf("%d", &t);
-	
-	while(t--)
-	{
-		scanf("%d", &a);
-		
-		pf(a);
-	}
+	primeSieve();
+    
+    int t, n;
+    
+    cin >> t;
+    
+    while(t--)
+    {
+		cin >> n;
+		div(n);
+	}	
 	
 	return 0;
 }
