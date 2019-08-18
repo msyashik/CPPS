99b38703223ef8881672d88777300f84c7259e10
msyashik <44289812+msyashik@users.noreply.github.com>
2019-08-18 23:56:31 +0600

    Create abc138_a.cpp

---
+++ Atcoder/abc138_a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long 
+#define Fastest ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+ 
+int main()
+{
+    int a;
+    string s;
+    
+    cin >> a >> s;
+    
+    if(a < 3200)
+    {
+		cout << "red\n";
+	}
+	else cout << s << endl;
+    	
+	return 0;
+}
