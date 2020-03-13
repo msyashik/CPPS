8b114bfab1c386526f5b24aa09ee73de5482fc61
msyashik <44289812+msyashik@users.noreply.github.com>
2020-03-13 17:47:05 +0600

    Create 1044 - Palindrome Partitioning.cpp

---
+++ LightOJ/1044 - Palindrome Partitioning.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+#define IOS ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define MX 100005
+#define SIZE 1005
+//
+char ch[SIZE];
+int dp[SIZE][SIZE], dp2[SIZE][SIZE];
+
+int func(int l, int r)
+{
+	if(l >= r) return 1;
+	if(dp[l][r] != -1) return dp[l][r];
+	if(ch[l] == ch[r] && func(l+1,r-1))
+	{
+	    return dp[l][r] = 1;    	
+	}
+	return dp[l][r] = 0;
+}
+
+int split(int left, int right)
+{
+	if(left == right) return 1;
+	if(left > right) return 0;
+	if(dp2[left][right] != -1) return dp2[left][right];
+	int ans = 100000;
+	for(int i = left; i <= right; i++)
+	{
+		if(func(left,i))
+		{
+			ans = min(ans,1 + split(i+1,right));
+		}
+	} 
+	return dp2[left][right] = ans;
+}
+
+int main()
+{
+	int t;
+	
+	scanf("%d", &t);
+	for(int kase = 1; kase <= t;kase++)
+	{
+	    scanf("%s", ch);
+	    int len = strlen(ch);
+	    memset(dp,-1,sizeof(dp)); 
+	    int ans = func(0,len-1);
+	    memset(dp2,-1,sizeof(dp2));
+	    int total = split(0,len-1);
+	    printf("Case %d: %d\n",kase,total);         
+	}
+	
+	return 0;
+}
