628ae33b35b92a13c37e5023cf532e238e5ebf27
Md.Shahariar Younus Ashik <mdshahariaryounusashik@gmail.com>
2020-04-20 17:13:06 +0600

    Create 1349. Farm.cpp

---
+++ Timus/1349. Farm.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long 
+#define endl '\n'
+
+int main()
+{
+	int a;
+	
+	cin >> a;
+	if(a == 1) cout<< "1 2 3\n";
+	else if(a == 2) cout<<"3 4 5\n";
+	else cout << -1 << endl;
+	return 0;
+}
