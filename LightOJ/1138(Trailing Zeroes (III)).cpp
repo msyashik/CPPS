71233cb7eaac30107929a78e924e74e10cb3c7a0
Md.Shahariar Younus Ashik <mdshahariaryounusashik@gmail.com>
2020-04-28 15:39:42 +0600

    Update 1138(Trailing Zeroes (III)).cpp

--- LightOJ/1138(Trailing Zeroes (III)).cpp
+++ LightOJ/1138(Trailing Zeroes (III)).cpp
@@ -1,70 +1,58 @@
 #include <bits/stdc++.h>
 
 using namespace std;
- 
-#define ll long long 
-#define IOS ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define endl "\n"
 
-ll coun = 0;
-void func(ll n, ll p)
+#define ll long long 
+#define Fastest ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define SIZE 1005
+#define eps 1e-9
+
+ll func(ll n)
 {
-	coun = 0;
+	ll twos = 0, x = n;
+	while(x)
+	{
+		twos += x/2;
+		x/= 2;
+	}
+	ll fives = 0;
 	while(n)
 	{
-        coun += (n/p);
-        n /= p;		
+		fives += n/5;
+		n/=5;
 	}
+	//cout << twos << " " << fives << endl;
+	return min(twos,fives);
 }
 
 int main()
-{
-	//
-	ll test, q;
-	scanf("%lld", &test);
-	for(ll kase = 1; kase <= test; kase++)
+{	
+	ll t, n;
+	
+	scanf("%lld", &t);
+	for(ll kase = 1; kase <= t; kase++)
 	{
-		scanf("%lld", &q);
-	    ll first = 1, last = 1000000000, mid = 0, mn = 0;
-	    int f = 0;
-	    while(first <= last)
+	    scanf("%lld", &n);
+	    printf("Case %lld: ", kase);
+	    ll mid, lo = 1, hi = 1000000000, val = 1e16;
+	    bool yes = false;
+	    while(lo < hi)
 	    {
-			if(mn == q) {f = 1;break;}
-		    else if(mn < q)
-		    {
-				first = mid+1;
-			}
-			else if(mn > q)
+			mid = (lo + hi) / 2;
+			ll ans = func(mid);
+			if(ans == n) 
 			{
-				last = mid-1;
+				yes = true;
+				hi = mid;
+				val = min(val,mid);
 			}
-			mid = (first+last) / 2;
-	        func(mid,2);	
-	        ll coun2 =  coun;
-	        func(mid,5);
-	        ll coun5 = coun;
-	        mn = min(coun2, coun5);
-	    }
-	    if(f == 0) printf("Case %lld: impossible\n", kase);
-	    else
-	    {
-			ll low = 1, hi = mid, md;
-			ll ans = 0;
-			ll ash = mid;
-			while(low <= hi)
-			{
-				md = (hi+low)/2;
-				func(md,2);
-				ll coun22 = coun;
-				func(md, 5);
-				ll coun55 = coun;
-				ans = min(coun22, coun55);
-				if(ans == q) ash = min(ash, md);
-				if(ans >= q) hi = md-1;
-				else low = md+1;
-			}
-			printf("Case %lld: %lld\n", kase, ash);
+			else if(ans < n) lo = mid + 1;
+			else if(ans > n) hi = mid;
 		}
+		if(yes) printf("%lld\n",val);
+		else printf("impossible\n");	
 	}
+	
 	return 0;
 }
