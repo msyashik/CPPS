808cd49d0e71e56f9299bb9ac03527317abc74e8
Md.Shahariar Younus Ashik <mdshahariaryounusashik@gmail.com>
2020-05-11 21:17:19 +0600

    Update Vasya and Petya's Game.cpp

--- Junior Training Sheet/C1/Vasya and Petya's Game.cpp
+++ Junior Training Sheet/C1/Vasya and Petya's Game.cpp
@@ -1,43 +1,63 @@
 #include <bits/stdc++.h>
 
 using namespace std;
- 
+
 #define ll long long 
-#define IOS ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define endl "\n"
+#define Coming ios_base :: sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-vector<int>v[100005];
-int n, c, arr[100005], vis[100005];
-int coun = 0;
+int prime[1010];
 
-void dfs(int node, int prev, int cats)
+void isprime()
 {
-	if(arr[node] == 0) cats = 0;
-	else cats++;
-	if(cats > c) return;
+	int len = sqrt(1000);
 	
-	vis[node] = 1;
-	int len = v[node].size();
-	if(len == 1 && v[node][0] == prev) {coun++;return;}
-	for(int i = 0; i < len ; i++)
+	prime[1] = 1;
+	
+	for(int i = 2; i <= len; i++)
 	{
-		int new_node = v[node][i];
-		if(!vis[new_node]) dfs(new_node, node, cats);
-	}
+		if(prime[i] == 0)
+		{
+			for(int j = 2*i; j <= 1000; j+=i)
+			{
+				prime[j] = 1;
+			}
+		}
+	} 
 }
 
+
 int main()
-{   
-    cin >> n >> c;
-    for(int i = 1;i <= n; i++) cin >> arr[i];
-    for(int i = 1;i <= n-1; i++)
-    {
-		int a, b;
-		cin >> a >> b;
-		v[a].push_back(b);
-		v[b].push_back(a);
-	}	
-	dfs(1,0,0);
-	cout << coun << endl;
+{
+    isprime();	
+    	
+	int n;
+	
+	cin >> n;
+	
+	set<int>s;
+	
+	for(int i = 1; i <= n; i++)
+	{
+		if(prime[i] == 0)
+		{
+			int p = 1;
+			
+			while(p*i <= n)
+			{
+				p  = p*i;
+			    s.insert(p);
+			}
+		}
+	}
+	
+	
+	cout << s.size() << "\n";
+	
+	for(auto q : s)
+	{
+		cout << q << " ";
+	}
+	cout << "\n";
+	
 	return 0;
 }
