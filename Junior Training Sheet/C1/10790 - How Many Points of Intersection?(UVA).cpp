b8c125f5783beb1e90c917c6ee80c749aed3a373
Md.Shahariar Younus Ashik <mdshahariaryounusashik@gmail.com>
2020-06-11 11:57:26 +0600

    Update 10790 - How Many Points of Intersection?(UVA).cpp

--- Junior Training Sheet/C1/10790 - How Many Points of Intersection?(UVA).cpp
+++ Junior Training Sheet/C1/10790 - How Many Points of Intersection?(UVA).cpp
@@ -7,6 +7,19 @@ using namespace std;
 #define endl '\n'
 #define IOS ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
+/* To make a cross point i have to select 2 points from the upper row and 2 points from the down row.
+                                           .     .
+                                            \   /
+                                             \ /
+                                              \
+                                             / \       
+                                            /   \
+                                           .     .
+
+Now i can select 2 points from row a in (aC2) ways and 2 points from row b in (bC2) ways.So the answer is aC2*bC2.
+If we iterate further we will get (a*(a-1))/2 * (b*(b-1))/2.
+
+*/
 int32_t main()
 {
 	int a,b;
